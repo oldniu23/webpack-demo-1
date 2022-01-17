5f9068da4f7c474505e3918847426dd1c871900c
oldniu <1241677287@qq.com>
2022-01-17 11:51:13 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
 var path = require("path");
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[contenthash].js",
+    // path: path.resolve(__dirname, "dist"),
+    filename: "index.[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "二哥",
+      // filename: "assets/admin.html",
+      template: "src/assets/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
