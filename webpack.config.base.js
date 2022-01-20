e741ebe96589d55bf38e5d140d54e0f4e7e4a099
oldniu <1241677287@qq.com>
2022-01-20 12:08:20 +0800

    准备上传了

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -13,4 +13,40 @@ module.exports = {
       template: "src/assets/index.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
+        test: /\.styl$/,
+        loader: ["style-loader", "css-loader", "stylus-loader"],
+        //stylus-loader把stylus代码变成css代码
+        // css-loader把css代码变成js代码
+        // style-loader把js变成style标签
+      },
+      {
+        test: /\.less$/i,
+        loader: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("dart-sass"),
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
